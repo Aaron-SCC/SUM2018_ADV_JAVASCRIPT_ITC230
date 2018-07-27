89b18b2e7221fa813187b068eda99d756343eb1a
Aaron-SCC <32554879+Aaron-SCC@users.noreply.github.com>
2018-07-26 20:54:42 -0700

    Update index.js

--- ASSIGNMENT04/index.js
+++ ASSIGNMENT04/index.js
@@ -1,6 +1,12 @@
 'use strict'
 
-let movies = require("./lib/module.js");
+let movies = require("./lib/book_module.js");
+
+var bookMethods = require("./lib/book_module");
+
+var Book = require("./models/book"); // use database model
+
+
 
 const express = require("express");
 const app = express();
@@ -13,31 +19,56 @@ let handlebars =  require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html'}));
 app.set("view engine", ".html");
 
-// send static file as response
-app.get('/', function(req,res){
-    res.type('text/html');
-    res.sendFile(__dirname + '/public/home.html'); 
+
+// GET ALL DEFAULT TO WEB PAGE
+app.get('/', (req, res, next) => {
+  bookMethods.getAll().then((items) => {
+    res.render('home', {books: items }); 
+  }).catch((err) =>{
+    return next(err);
+  });
 });
 
+
+// FOR SHOWING DETAILS
+app.get('/details', (req,res,next) => {
+    Book.findOne({ title:req.query.title }, (err, book) => {
+        if (err) return next(err);
+        res.type('text/html');
+        res.render('details', {result: book} ); 
+    });
+});
+
+// FOR SHOWING DETAILS
+app.post('/details', (req,res, next) => {
+    Book.findOne({ title:req.body.title }, (err, book) => {
+        if (err) return next(err);
+        res.type('text/html');
+        res.render('details', {result: book} ); 
+    });
+});
+
+
+// FOR DELETING
+app.get('/delete', (req,res, next) => { // FLAG A
+    Book.remove({ title:req.query.title }, (err, result) => { // FLAG B
+        if (err) return next(err);
+        let deleted = result.n !== 0; // n will be 0 if no docs deleted
+        Book.count({},  (err, total) => {  // FLAG C                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ) => {
+             res.type('text/html');
+             res.render('delete', {title: req.query.title, deleted:deleted , total:total } );    
+        });  // FLAG C
+    });  // FLAG B
+});  // FLAG A
+
+
 // send plain text response
+// for the ABOUT PAGE
 app.get('/about', function(req,res){
     res.type('text/plain');
     res.send('About page');
 });
 
-// handle GET 
-app.get('/delete', function(req,res){
-    let result = movies.delete(req.query.title); // delete movies object
-    res.render('delete', {title: req.query.title, result: result});
-});
-
-// handle POST
-app.post('/get', function(req,res){
-    console.log(req.body)
-    var header = 'Searching for: ' + req.body.title + '<br>';
-    var found = movies.get(req.body.title);
-    res.render("details", {title: req.body.title, result: found});
-});
 
 // define 404 handler
 app.use(function(req,res) {
@@ -45,6 +76,7 @@ app.use(function(req,res) {
     res.status(404);
     res.send('404 - Not found');
 });
+
 
 app.listen(app.get('port'), function() {
     console.log('Express started');    
